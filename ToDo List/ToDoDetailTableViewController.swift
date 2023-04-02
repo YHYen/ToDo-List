d8e0fd9dbf4c9de4ce6086dbe85f5b927d76794f
YHYen <cloudyhyen@gmail.com>
2023-04-02 14:23:01 +0800

    create tableView in main viewController and add tableView controller to transfer data. complete add data function and update data function

---
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDo List
+//
+//  Created by 顏逸修 on 2023/4/2.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    var toDoItem: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if toDoItem == nil {
+            toDoItem = ""
+        }
+        nameField.text = toDoItem
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text
+    }
+    
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
---
+++ ToDo List/ToDoListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by 顏逸修 on 2023/4/1.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Learn Objecttive-C", "Learn Angular", "Build Apps"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let destination = segue.destination as! ToDoDetailTableViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.toDoItem = toDoArray[selectedIndexPath.row]
+        } else if segue.identifier == "AddDetail" {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
+    }
+    
+    
+    @IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
+        let source  = segue.source as! ToDoDetailTableViewController
+        if let selectIndexPath = tableView.indexPathForSelectedRow {
+            toDoArray[selectIndexPath.row] = source.toDoItem
+            tableView.reloadRows(at: [selectIndexPath], with: .automatic)
+        } else {
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(source.toDoItem)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+            tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
+        }
+    }
+}
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // print("numberOfRowsInSection was just called. Returning \(toDoArray.count)")
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is cell containing \(toDoArray[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
